967f321b87ef9be96042cd1a5b2762b67c98fe23
¨Leonardo <negreirosleo12@gmail.com>
2024-05-28 00:09:53 -0300

    Create server action to create Posts

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -5,13 +5,32 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
+  DialogTrigger,
+  DialogClose
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 
 export const CreatePost = () => {
+  async function createPost(formData: FormData) {
+    'use server'
+
+    const rawFormData = {
+      title: formData.get('title'),
+      body: formData.get('body'),
+      views: 0
+    }
+
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      body: JSON.stringify(rawFormData),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -22,7 +41,7 @@ export const CreatePost = () => {
           <DialogTitle>Create a new Post</DialogTitle>
           <DialogDescription></DialogDescription>
         </DialogHeader>
-        <form className="grid gap-4 py-4">
+        <form action={createPost} className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center align gap-4">
             <Label htmlFor="title" className="text-right">
               Title
@@ -36,7 +55,9 @@ export const CreatePost = () => {
             <Textarea id="body" name="body" className="col-span-3" placeholder="Post content." />
           </div>
           <div className="flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2">
-            <Button type="submit">Create Post</Button>
+            <DialogClose asChild>
+              <Button type="submit">Create Post</Button>
+            </DialogClose>
           </div>
         </form>
       </DialogContent>
